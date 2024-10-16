670009b3fefdc13d4338be869e62b0c399a8b272
Abhigyan Verma <abhigyanverma111@gmail.com>
2024-10-16 20:18:08 +0530

    sample api (not trial)

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,3 +17,23 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+
+// Initialize Firebase Admin (optional, but necessary if using Firestore or other Firebase services)
+admin.initializeApp();
+
+const app = express();
+
+// Example middleware or route configuration (keep your existing routes here)
+app.get("/api/hello", (req, res) => {
+  res.send("Hello from Firebase!");
+});
+
+// Other routes in your original Express app
+// app.use('/api', yourRoutes);
+
+// Export the Express app as a Firebase Cloud Function
+exports.api = functions.https.onRequest(app);
